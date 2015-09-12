9d22721e5ede2ea8a7dc288e6d5ff8faf8475f21
notalbanian <samuel.b.lauber@gmail.com>
2015-09-12 14:01:28 -0500

    views

--- articles/views.py
+++ articles/views.py
@@ -1,18 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Category, Article, Resource
 
 def landing(request):
-    return render(request, "landing.html")
+    return render(request, "landing.html", {'categories': Category.objects.all()})
 
 def search(request):
     query = request.POST['query']
     # TODO: use an actual search engine
-    articles = Article.objects.all()
-    resources = Resource.objects.all()
+    articles = (Article.objects.filter(title__icontains=query) | Article.objects.filter(text__icontains=query)).all()
+    resources = (Resource.objects.filter(name__icontains=query) | Resource.objects.filter(description__icontains=query)).all()
     return render(request, "results.html", {'articles': articles, 'resources': resources})
 
 def article(request, id):
-    pass
+    article = get_object_or_404(Article, pk=id)
+    return render(request, "article.html", {'article': article})
 
 def resource(request, id):
     pass
