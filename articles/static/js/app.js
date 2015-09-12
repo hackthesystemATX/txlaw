78c60fba91a8bc9fbf5c498add45c12a25af3cd8
Cerita <ceritakelly@gmail.com>
2015-09-12 12:35:10 -0500

    Interface
    Bootstrap, Index.html

---
+++ articles/static/js/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	alert("JQuery is working!");
+
+//Submit post
+
+
+});
+
+
+$("#fillquery").submit(function() {
+
+	alert("submitted");
+	event.preventDefault();
+
+});
