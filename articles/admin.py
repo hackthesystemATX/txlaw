02403cd4864544dc680f2679300aa0058c9adfb3
sbl <samuel.b.lauber@gmail.com>
2015-09-12 19:38:04 +0000

    stuff

--- articles/admin.py
+++ articles/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
-from .models import Category, Article, Resource
+from django.contrib.auth.models import User
+from .models import Category, Article, Resource, Profile
+
+class ProfileInline(admin.StackedInline):
+	model = Profile
+	can_delete = False
+	verbose_name_plural = 'profile'
+
+
+class UserAdmin(UserAdmin):
+	inlines = (ProfileInline,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 
 admin.site.register(Category)
 admin.site.register(Article)
